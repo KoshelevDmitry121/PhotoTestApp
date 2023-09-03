77fae1b78ad86d361ff7b55443ff44d01afc2855
Dmitry Koshelev <d.koshelev@ltst.ru>
2023-09-03 20:09:18 +0600

    Add network error alert

--- PhotoTestApp/Services/NetworkService.swift
+++ PhotoTestApp/Services/NetworkService.swift
@@ -5,7 +5,7 @@
 //  Created by Dmitry Koshelev on 02.09.2023.
 //
 
-import Foundation
+import UIKit
 
 enum NetworkHTTPMethod: String {
     case options = "OPTIONS"
@@ -36,16 +36,46 @@ struct NetworkService: NetworkServiceInterface {
         var request = URLRequest(url: url)
         request.httpMethod = target.httpMethod.rawValue
         
-        guard let (data, response) = try? await URLSession.shared.data(for: request),
-              let response = response as? HTTPURLResponse else { return nil }
-        
-        let statusCode = response.statusCode
-        switch statusCode {
-        case 200..<299:
-            return try? JSONDecoder().decode(T.self, from: data)
-        default:
+        do {
+            let (data, response) = try await URLSession.shared.data(for: request)
+            guard let response = response as? HTTPURLResponse else { return nil }
+            let statusCode = response.statusCode
+            
+            switch statusCode {
+            case 200..<299:
+                do {
+                    return try JSONDecoder().decode(T.self, from: data)
+                } catch {
+                    await showError(title: "Error", message: error.localizedDescription)
+                    return nil
+                }
+            default:
+                await showError(title: "Error", message: "statusCode - \(statusCode)")
+                return nil
+            }
+        } catch {
+            await showError(title: "Error", message: error.localizedDescription)
             return nil
         }
+    }
+    
+    private func showError(title: String, message: String) async {
+        let alertController = await UIAlertController(title: title, message: message, preferredStyle: .alert)
+        await alertController.addAction(
+            UIAlertAction(
+                title: "Ok",
+                style: .cancel
+            )
+        )
+        
+        var rootViewController = await UIApplication.shared.keyWindow?.rootViewController
+        if let navigationController = rootViewController as? UINavigationController {
+            rootViewController = await navigationController.viewControllers.first
+        }
+        if let tabBarController = rootViewController as? UITabBarController {
+            rootViewController = await tabBarController.selectedViewController
+        }
+        await rootViewController?.present(alertController, animated: true, completion: nil)
     }
     
 }
